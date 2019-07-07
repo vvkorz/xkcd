dd327bd053963d48ba4c53b1c93851d38aaa54db
vvkorz <korzinovvv@gmail.com>
2019-07-07 09:19:43 +0200

    first working version

--- xkcd.sh
+++ xkcd.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-curl https://xkcd.com/1/info.0.json | grep -Po '(?<=\"img": )(.*?)(?=\,)' > temp.txt 
+
+# If you need a random int within a certain range, use the 'modulo' operator.
+# This returns the remainder of a division operation.
+
+RANGE=1500
+#while true; do
+for item in 1 2 3 4 5
+do
+number=$RANDOM # random image id
+let "number %= $RANGE"
+foo="https://xkcd.com/"
+url="$foo$number/info.0.json"
+echo $url
+curl $url | grep -Po '(?<=\"img": )(.*?)(?=\,)' > temp.txt 
 python xkcd.py
 curl $(python xkcd.py) -o image 
-firefox image
+feh -F image &
+sleep 5
+kill $!
+done
